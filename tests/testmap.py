bb729fdb23d445f8543ce1882aaec56b4aafbe7c
hoffmannm <mjhoffmann@gmail.com>
2010-08-30 08:24:46 +0200

    Generalized test for lattice mapping

--- tests/testmap.py
+++ tests/testmap.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 
+import sys
+sys.path.append('../fortran_src')
 import run_kmc, kmc
 
 r = run_kmc.KMC_Run()
-for i in range(1,1+kmc.base.get_volume()):
-    site = kmc.lattice.nr2ruo2(i)
-    j = kmc.lattice.ruo22nr(site)
-    print(i,site,j)
-    if i != j :
-        print("WRONG")
-        break
-else:
-    print("OLL KLEAR")
+for lattice in r.lattice_names:
+    for i in range(1,1+kmc.base.get_volume()):
+        site = eval('kmc.lattice.nr2%s(i)' % lattice)
+        j = eval('kmc.lattice.%s2nr(site)' % lattice)
+        print(i,site,j)
+        if i != j :
+            print("WRONG")
+            break
+    else:
+        print("OLL KLEAR")
         
