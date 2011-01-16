d9d2b0dc34323817e43257233347be0f2a2d94ba
Max J. Hoffmann <mjhoffmann@gmail.com>
2011-01-16 16:37:41 +0000

    Changed testmap test to take eval out of tight loop

--- tests/testmap.py
+++ tests/testmap.py
@@ -6,9 +6,11 @@ import run_kmc, kmc
 
 r = run_kmc.KMC_Run()
 for lattice in r.lattice_names:
-    for i in range(1,1+kmc.base.get_volume()):
-        site = eval('kmc.lattice.nr2%s(i)' % lattice)
-        j = eval('kmc.lattice.%s2nr(site)' % lattice)
+    nr2lattice =  eval('kmc.lattice.nr2%s' % lattice)
+    lattice2nr =  eval('kmc.lattice.%s2nr' % lattice)
+    for i in xrange(1,1+kmc.base.get_volume()):
+        site = nr2lattice(i)
+        j = lattice2nr(site)
         print(i,site,j)
         if i != j :
             print("WRONG")
