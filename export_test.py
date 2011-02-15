e0b07a79a47d8e385879eb0e58e779280b1a9b11
Max Hoffmann <hoffmann@boomerangutan.(none)>
2011-02-15 01:01:43 +0100

    Added export test client

---
+++ export_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import main
+import shutil
+
+export_dir = 'default_src'
+xml_file = 'empty.xml'
+
+shutil.rmtree(export_dir,ignore_errors=True)
+kmc = main.KMC_Editor()
+kmc.project_tree.import_xml_file(xml_file)
+
+kmc.on_btn_export_src__clicked('',export_dir)
+
+exit()
