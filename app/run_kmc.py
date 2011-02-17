c44dad74d54d11eac06e5641264e1df7e8c6821e
hoffmannm <mjhoffmann@gmail.com>
2011-02-17 19:50:03 +0100

    Cleaned run_kmc a bit

--- app/run_kmc.py
+++ app/run_kmc.py
@@ -1,10 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/ipython
 """Generical kMC steering script for binary modules created kmos"""
 
 
-from kmc import lattice, proclist
+import numpy as np
+import ase
+from kmc import base, lattice, proclist
 
-proclist.init((10,)*int(lattice.model_dimension),'foobar', lattice.default_layer, proclist.default_species)
-print("Finished initialization")
-for i in xrange(int(3.e6)):
-    proclist.do_kmc_step()
+class KMC_Model():
+    def __init__(self, size=10, system_name='kmc_model'):
+        proclist.init((size,)*int(lattice.model_dimension),system_name, lattice.default_layer, proclist.default_species)
+        self.cell_size = np.dot(lattice.unit_cell_size, lattice.system_size)
+
+    def __del__(self):
+        base.deallocate_system()
+            
+
+    def run_steps(self, n):
+        for i in xrange(n):
+            proclist.do_kmc_step()
+
+        
+if __name__ == '__main__':
+    model  = KMC_Model()
