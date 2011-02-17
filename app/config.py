f1d67f0c28f1185ed4d3a3f0d0f41b97914cfdcd
hoffmannm <mjhoffmann@gmail.com>
2011-02-17 18:00:43 +0100

    Stabilized glade file import

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
+import kiwi
 
 APP_ABS_PATH = os.path.dirname(os.path.abspath(__file__))
 GLADEFILE = os.path.join(APP_ABS_PATH, 'kmc_editor.glade')
-
+kiwi.environ.environ.add_resource('glade', APP_ABS_PATH)
