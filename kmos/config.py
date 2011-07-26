e8bf37223dea81e20142ace413409285cae0d97c
hoffmannm <mjhoffmann@gmail.com>
2011-07-26 23:41:23 +0200

    kmos-gui starts completely from distutils install

--- kmos/config.py
+++ kmos/config.py
@@ -1,7 +1,7 @@
 import os
 import kiwi
 
-#APP_ABS_PATH = os.path.dirname(os.path.abspath(__file__))
-#GLADEFILE = os.path.join(APP_ABS_PATH, 'kmc_editor.glade')
-GLADEFILE = 'kmc_editor.glade'
-#kiwi.environ.environ.add_resource('glade', APP_ABS_PATH)
+APP_ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+GLADEFILE = os.path.join(APP_ABS_PATH, 'kmc_editor.glade')
+GLADEFILE = 'kmos/kmc_editor.glade'
+kiwi.environ.environ.add_resource('glade', APP_ABS_PATH)
--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ from distutils.core import setup
 
 author = 'Max J. Hoffmann'
 author_email = 'mjhoffmann@gmail.com'
-data_files = [
-             ('f90sources','f90sources/*'), 
-        
-             ]
 description = "kMC modeling on steroids"
 license = 'COPYING'
 long_description = "A vigorous attempt to make lattice kinetic Monte Carlo modelling more accessible"
@@ -15,15 +11,17 @@ packages = [
            'kmos',
            'kmos.pygtkcanvas',
            ]
-name='python-kmos'
-package_dir = {'kmos':'kmos',
-               }
+package_dir = {'kmos':'kmos'}
+package_data = {'kmos':['fortran_src/*f90',
+                        'kmc_editor.glade',
+                        'kmc_project_v0.2.dtd']}
 platforms = ['linux']
+name='python-kmos'
 scripts = [
         'tools/kmos-gui',
         'tools/kmos-export-src',
         'tools/kmos-build',
-        'tools/kmos-f2py',
+        'tools/kmos-build-standalone',
         ]
 url = 'https://github.com/mhoffman/kmos'
 version = '0.1'
@@ -31,13 +29,13 @@ version = '0.1'
 setup(
       author=author,
       author_email=author_email,
-      data_files=data_files,
       description=description,
       license=license,
       long_description=long_description,
       name=name,
       packages=packages,
       package_dir=package_dir,
+      package_data=package_data,
       platforms=platforms,
       scripts=scripts,
       url=url,
