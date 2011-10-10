b1e343069dcdf799a88e61e396432898c97bd331
hoffmannm <mjhoffmann@gmail.com>
2011-10-10 08:24:01 -0300

    Adapted export test program to changes in namespace

--- tests/export_test/test_export.py
+++ tests/export_test/test_export.py
@@ -5,10 +5,10 @@ import os.path, shutil
 import filecmp
 
 TEST_DIR = 'test_export'
-if os.path.exists(TEST_DIR):
-    shutil.rmtree(TEST_DIR)
+#if os.path.exists(TEST_DIR):
+    #shutil.rmtree(TEST_DIR)
 
-os.system('../../main.py -o default.xml -x %s' % TEST_DIR)
+os.system('kmos export default.xml %s' % TEST_DIR)
 
 cmp = filecmp.cmp('reference_test/proclist.f90', '%s/proclist.f90' % TEST_DIR)
 if cmp:
