aa3dcdf9819db95e563504637bbdf449c32cd71c
hoffmannm <mjhoffmann@gmail.com>
2011-10-14 11:20:22 -0300

    Cleaned up test/export_test a little

--- tests/export_test/test_export.py
+++ tests/export_test/test_export.py
@@ -12,6 +12,7 @@ def test_import_export():
     os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
     TEST_DIR = 'test_export'
+    REFERENCE_DIR = 'reference_export'
     #if os.path.exists(TEST_DIR):
         #shutil.rmtree(TEST_DIR)
 
@@ -19,7 +20,7 @@ def test_import_export():
     pt.import_xml_file('default.xml')
     kmos.export.export_source(pt, TEST_DIR)
     for filename in ['base', 'lattice', 'proclist']:
-        assert filecmp.cmp(os.path.join('reference_test', '%s.f90' % filename),
+        assert filecmp.cmp(os.path.join(REFERENCE_DIR, '%s.f90' % filename),
                           os.path.join(TEST_DIR, '%s.f90' % filename))
 
     os.chdir(cwd)
