f5c137544f5ad2927681893c8bb38ccb9adb2609
hoffmannm <mjhoffmann@gmail.com>
2011-11-25 12:45:52 -0200

    Added script which creates *rst docs via robodoc from *f90 source

---
+++ kmos/fortran_src/ascii2rst.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+
+import os
+
+os.system('robodoc --src base.f90 --doc base --singlefile --ascii')
+
+asci = file('base.txt', 'r').readlines()
+
+
+new_asci = []
+jump = 0
+for i, line in enumerate(asci):
+    if jump:
+        jump += -1
+        continue
+    elif 'FUNCTION' in line:
+        continue
+    elif 'ARGUMENTS' in line:
+        new_asci.append('\nArguments\n')
+        new_asci.append('^^^^^^^^^\n')
+    elif i == len(asci) - 1:
+        continue
+    elif '------' in line:
+        new_asci.append(asci[i+1])
+        new_asci.append(line)
+        jump = 1
+    else:
+        new_asci.append(line)
+    
+
+
+
+rst = file('base.rst', 'w')
+for line in new_asci:
+    rst.write(line)
+rst.close()
+
+
+os.system('rst2html base.rst > base.html')
