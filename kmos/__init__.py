c8bc4157027ab6af69b8c15c39e7982f75416485
hoffmannm <mjhoffmann@gmail.com>
2012-04-23 21:16:00 +0200

    Bumped version

--- kmos/__init__.py
+++ kmos/__init__.py
@@ -50,7 +50,7 @@ or it may be used as an API via the kmos module.
 #import kmos.types
 #import kmos.io
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 
 def evaluate_rate_expression(rate_expr, parameters={}):
