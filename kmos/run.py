d6ae23b22ae169e793baaf82e077a7b4ae6b1013
hoffmannm <mjhoffmann@gmail.com>
2012-08-13 10:22:59 +0200

    Most likely found source of error in touchup: forgot updating accum_rate

--- kmos/run.py
+++ kmos/run.py
@@ -182,7 +182,8 @@ class KMC_Model(multiprocessing.Process):
         else:
             self.lattice_representation = Atoms()
         set_rate_constants(settings.parameters, self.print_rates)
-        self.base.update_accum_rate()
+        # DEBUGGING, do we need this?
+        #self.base.update_accum_rate()
 
     def __repr__(self):
         """Print short summary of current parameters and rate
@@ -683,6 +684,7 @@ class KMC_Model(multiprocessing.Process):
                             site_name = self.settings.site_names[n].lower()
                             eval('self.proclist.touchup_%s([%i, %i, %i, %i])'
                                 % (site_name, x, y, z, n + 1))
+        self.base.update_accum_rate()
 
     def get_backend(self):
         if hasattr(self.proclist, 'backend'):
