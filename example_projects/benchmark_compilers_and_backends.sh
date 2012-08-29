069994c0225c888b30a946d730bca4366391731a
hoffmannm <mjhoffmann@gmail.com>
2012-08-29 14:30:03 +0200

    Put general benchmark script under version control

---
+++ example_projects/benchmark_compilers_and_backends.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -u
+
+# TODO: Very experimental status
+#       Translate to python and make
+#       more flexible.
+
+
+model=${1}
+BENCHMARK_FILE="$(readlink -f benchmark_${model}.txt)"
+for compiler in intelem gfortran
+do
+    for backend in local_smart lat_int
+    do
+        export F2PY_FCOMPILER=${compiler}
+        rm -rf ${model}
+        echo -e "BENCHMARK ${model} ${backend} ${compiler}" >> ${BENCHMARK_FILE}
+        echo "    COMPILE TIME" >> ${BENCHMARK_FILE}
+        /usr/bin/time -o ${BENCHMARK_FILE} -a kmos export -b ${backend} ${model}.xml
+        cp kmc_settings.py ${model}
+        cp setup_model.py ${model}
+        cd ${model}
+        echo "    RUN TIME" >> ${BENCHMARK_FILE}
+        for i in $(seq 3)
+        do
+            kmos benchmark >> ${BENCHMARK_FILE}
+        done
+        cd ..
+        echo -e "\n\n\n" >> ${BENCHMARK_FILE}
+    done
+done
