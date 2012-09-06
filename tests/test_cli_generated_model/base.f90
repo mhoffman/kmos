6419087e6ebd48261643005a42dfc14d0a55b897
hoffmannm <mjhoffmann@gmail.com>
2012-09-06 18:52:00 +0200

    ok, now

--- tests/test_cli_generated_model/base.f90
+++ tests/test_cli_generated_model/base.f90
@@ -72,14 +72,13 @@ public :: add_proc, &
     reload_system, &
     reset_site, &
     save_system, &
-    set_null_species, &
     set_rate_const, &
     update_accum_rate, &
     update_clocks
 
 
 ! Public constants
-integer(kind=iint) :: null_species = -1
+integer(kind=iint), parameter :: null_species = -1
 
 !---- Allocatable, module wide, variables
 integer(kind=iint), dimension(:,:,:), allocatable, public :: avail_sites
@@ -1278,12 +1277,5 @@ subroutine assertion_fail(a, r)
     stop
 
 end subroutine assertion_fail
-
-subroutine set_null_species(input_null_species)
-    integer(kind=iint), intent(in) :: input_null_species
-
-    null_species = input_null_species
-
-end subroutine set_null_species
 
 end module base
