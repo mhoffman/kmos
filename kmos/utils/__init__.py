mhoffman/kmos

commit c00a87ad0f5d7d8dfe8beca768e26ccd1c4ca589

Sped up those cases of evalate_kind_value, where there is nothing to evaluate

kmos/utils/__init__.py
@@ -187,7 +187,16 @@ def evaluate_kind_values(infile, outfile):
     import re
     import os
     import sys
+    import shutil
     sys.path.append(os.path.abspath(os.curdir))
+
+    with open(infile) as infh:
+        intext = infh.read()
+    if not ('select_int_kind' in intext
+            or 'selected_real_kind' in intext):
+        shutil.copy(infile, outfile)
+        return
+
 
     def import_selected_kind():
         """Tries to import the module which provides

commit 9704bf1a6ccfa9c8c512c823f66cdc5392d795ca

Fixed bug introduced in c00a87a, that skip evaluation of kind_values

kmos/utils/__init__.py
@@ -193,8 +193,8 @@ def evaluate_kind_values(infile, outfile):
 
     with open(infile) as infh:
         intext = infh.read()
-    if not ('select_int_kind' in intext
-            or 'selected_real_kind' in intext):
+    if not ('selected_int_kind' in intext.lower()
+            or 'selected_real_kind' in intext.lower()):
         shutil.copy(infile, outfile)
         return
 