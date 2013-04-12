a0c83c1c25a402915f62a9471e0966e5133a7df7
hoffmannm <mjhoffmann@gmail.com>
2013-04-12 18:23:59 +0200

    Added -fimplicit-none to gfortran call

--- kmos/utils/__init__.py
+++ kmos/utils/__init__.py
@@ -361,7 +361,7 @@ def build(options):
 
     extra_flags = {}
     extra_flags['gfortran'] = ('-ffree-line-length-none -ffree-form'
-                               ' -xf95-cpp-input -Wall -O3')
+                               ' -xf95-cpp-input -Wall -O3 -fimplicit-none')
     extra_flags['gnu95'] = extra_flags['gfortran']
     extra_flags['intel'] = '-fast -fpp -Wall -I/opt/intel/fc/10.1.018/lib'
     extra_flags['intelem'] = '-fast -fpp -Wall'
