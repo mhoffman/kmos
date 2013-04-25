63d23528ac039f815ec47c5da219896c269c186c
hoffmannm <mjhoffmann@gmail.com>
2013-04-25 11:28:55 +0200

    PEP8 cosmetics

--- examples/run.py
+++ examples/run.py
@@ -19,8 +19,11 @@ and ScanKinetics.lock should be moved out of the way.
 
 from kmos.run import ModelRunner, PressureParameter, TemperatureParameter
 
+
 class ScanKinetics(ModelRunner):
     p_O2gas = PressureParameter(1)
     T = TemperatureParameter(600)
     p_COgas = PressureParameter(min=1, max=10, steps=40)
+
+
 ScanKinetics().run(init_steps=1e7, sample_steps=1e7, cores=4)
